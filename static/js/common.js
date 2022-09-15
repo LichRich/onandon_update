418940746e085cf3320a2bf2e6467caf7397fb7d
LichRich <leech135789@gmail.com>
2022-09-15 10:36:57 +0900

    update
    1. loading page
    2. actions for resizing

--- static/js/common.js
+++ static/js/common.js
@@ -1,5 +1,35 @@
 $(document).ready(function () {
 
+    $(document).keydown(function (e) {
+        if(e.defaultPrevented) {
+            return;
+        }
+
+        var handled = false;
+
+        // Tab 방지
+        if(e.keyCode == 9) {
+            handled = true;
+        }
+        // Space Bar 방지
+        else if(e.keyCode == 32) {
+            handled = true;
+        }
+        // Block End Key
+        else if(e.keyCode == 35) {
+            handled = true;
+        }
+        // Block Home Key
+        else if(e.keyCode == 36) {
+            handled = true;
+        }
+
+        if(handled) {
+            console.log(e.keyCode);
+            e.preventDefault();
+        }
+    });
+
     $('.intro-contents').animate({
         paddingTop: '0px',
         opacity: 1
@@ -18,4 +48,15 @@ $(document).ready(function () {
         alert('준비중입니다!');
     });
 
+});
+
+// $(window).on('resize', function() {
+//     this.location.reload(false);
+// });
+
+/* ---------------------------------------------- /*
+ * Loading
+/* ---------------------------------------------- */
+$(window).on('load', function() {
+    $('.loading').hide();
 });
